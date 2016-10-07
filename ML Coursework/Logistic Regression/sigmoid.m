9e4c0238f78fe9c420006e2bef6804eed7f7c53c
vidsag <vidhyasagar.mohanraj@gmail.com>
2016-10-07 23:13:43 +0530

    Coursework

---
+++ ML Coursework/Logistic Regression/sigmoid.m
@@ -0,0 +1,10 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+g = zeros(size(z));
+negativeZ = -z;
+exponent = e .^ negativeZ;
+ones = ones(size(z));
+g = ones ./ (ones .+ (exponent));
+end
