e4aa2140d1b6db0bac16ed8cae8805270096abc6
vidsag <vidhyasagar.mohanraj@gmail.com>
2016-10-07 23:32:46 +0530

    Coursework

---
+++ ML Coursework/Digit Recognizer/randInitializeWeights.m
@@ -0,0 +1,13 @@
+function W = randInitializeWeights(L_in, L_out)
+%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+%incoming connections and L_out outgoing connections
+%   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
+%   of a layer with L_in incoming connections and L_out outgoing 
+%   connections. 
+
+
+W = zeros(L_out, 1 + L_in);
+
+epsilon = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon - epsilon;
+end
---
+++ ML Coursework/Digit Recognizer/sigmoidGradient.m
@@ -0,0 +1,11 @@
+function g = sigmoidGradient(z)
+%SIGMOIDGRADIENT returns the gradient of the sigmoid function
+%evaluated at z
+%   g = SIGMOIDGRADIENT(z) computes the gradient of the sigmoid function
+%   evaluated at z.
+
+g = zeros(size(z));
+
+sigmoid = sigmoid(z);
+g = sigmoid .* (1 .- sigmoid);
+end
